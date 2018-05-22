e9ab317028107372a9600bf39b5838c830a18405
Casey Marshall <casey.s.marshall@gmail.com>
2018-05-22 13:12:56 -0700

    Release tasks.

--- project.clj
+++ project.clj
@@ -6,4 +6,13 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/java.classpath "0.2.3"]
                  [org.clojure/tools.namespace "0.3.0-alpha4"]]
-  :profiles {:dev {:dependencies [[amazonica "0.3.121"]]}})
+  :profiles {:dev {:dependencies [[amazonica "0.3.121"]]}}
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version"
+                   "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag"]
+                  ["deploy" "clojars"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]])
