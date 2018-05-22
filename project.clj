4257388791e574c2604e8730f73c16b68c9bcc15
Casey Marshall <casey.s.marshall@gmail.com>
2018-05-22 13:46:30 -0700

    Version 0.1.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject locoroll/mockazonica "0.1.2"
+(defproject locoroll/mockazonica "0.1.3-SNAPSHOT"
   :description "Tool for mocking out amazonica"
   :url "https://github.com/csm/mockazonica"
   :license {:name "Eclipse Public License"
